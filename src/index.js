9592045a0c44051f14b2e418b1c01f84f86705ee
Ravi Kushwaha <98015488+devopsexpertlearning@users.noreply.github.com>
2025-10-05 13:15:24 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const PARAMETERS = {
 };
 
 // Endpoint for login redirects (should match Okta app settings)
-const LOGIN_ENDPOINT = '/login';
+const LOGIN_ENDPOINT = '/authorization-code/callback';
 
 // ====== UTILITY FUNCTIONS ======
 
